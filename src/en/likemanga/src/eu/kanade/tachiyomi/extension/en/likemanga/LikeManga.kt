3b04860cf8b4b65a71f0e6661d252dafffe671c6
Sky-Rdx <familygroup197@gmail.com>
2026-02-21 23:37:13 +0530

    Update LikeManga.kt

--- src/en/likemanga/src/eu/kanade/tachiyomi/extension/en/likemanga/LikeManga.kt
+++ src/en/likemanga/src/eu/kanade/tachiyomi/extension/en/likemanga/LikeManga.kt
@@ -2,7 +2,11 @@ package eu.kanade.tachiyomi.extension.en.likemanga
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.*
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
@@ -10,7 +14,7 @@ import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Locale
 
 class LikeManga : ParsedHttpSource() {
 
