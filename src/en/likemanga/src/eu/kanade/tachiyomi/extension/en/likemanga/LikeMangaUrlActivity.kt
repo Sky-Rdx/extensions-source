4a4ccb90d899bf6dd44cc4932746f8f27dc7ae3e
Sky-Rdx <familygroup197@gmail.com>
2026-02-22 00:35:33 +0530

    Update LikeMangaUrlActivity.kt

--- src/en/likemanga/src/eu/kanade/tachiyomi/extension/en/likemanga/LikeMangaUrlActivity.kt
+++ src/en/likemanga/src/eu/kanade/tachiyomi/extension/en/likemanga/LikeMangaUrlActivity.kt
@@ -1,35 +1,27 @@
 package eu.kanade.tachiyomi.extension.en.likemanga
 
 import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import kotlin.system.exitProcess
 
 class LikeMangaUrlActivity : Activity() {
-    private val tag = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val pathSegments = intent?.data?.pathSegments
-        if (pathSegments != null && pathSegments.size > 0) {
-            val mainIntent = Intent().apply {
-                action = "eu.kanade.tachiyomi.SEARCH"
-                putExtra("query", "${LikeManga.URL_SEARCH_PREFIX}${pathSegments[0]}")
-                putExtra("filter", packageName)
-            }
 
-            try {
-                startActivity(mainIntent)
-            } catch (e: ActivityNotFoundException) {
-                Log.e(tag, e.toString())
-            }
-        } else {
-            Log.e(tag, "could not parse uri from intent $intent")
+        val slug =
+            intent?.data?.lastPathSegment ?: return
+
+        val mainIntent = Intent().apply {
+            action = "eu.kanade.tachiyomi.SEARCH"
+            putExtra(
+                "query",
+                LikeManga.URL_SEARCH_PREFIX + slug,
+            )
+            putExtra("filter", packageName)
         }
 
+        startActivity(mainIntent)
         finish()
-        exitProcess(0)
     }
 }
